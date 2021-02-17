ead0c35b688a25a5b716cbc48107a6620bc5745f
joergbuchwald <joerg.buchwald@ufz.de>
2021-02-18 00:09:34 +0100

    add old version

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""NetCDFinterface: a python NetCDF API for AHM workflow"""
+
+from setuptools import setup, find_packages
+
+setup(name="NetCDFinterface",
+      version=0.01,
+      maintainer="Jörg Buchwald",
+      maintainer_email="joerg_buchwald@ufz.de",
+      author="Jörg Buchwald",
+      author_email="joerg.buchwald@ufz.de",
+      url="https://github.com/joergbuchwald/NetCDFinterface",
+      classifiers=["Intended Audience :: Science/Research",
+          "Topic :: Scientific/Engineering :: Visualization",
+          "Topic :: Scientific/Engineering :: Physics",
+          "Topic :: Scientific/Engineering :: Mathematics",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.8"],
+      license="MIT -  see LICENSE.txt",
+      platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+      include_package_data=True,
+      install_requires=["lxml", "vtk", "pandas", "scipy"],
+      py_modules=["netCDF"],
+      package_dir={'': 'src'})
